ae8fe09e3448728dd37973ec450b2397577024c4
Dylan Sleith <sleithdylan@gmail.com>
2023-11-18 13:23:30 +0000

    chore: update dotfiles

--- terminal/.hyper.js
+++ terminal/.hyper.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 // Future versions of Hyper may add additional config options,
 // which will not automatically be merged into this file.
 // See https://hyper.is#cfg for all currently supported options.
@@ -6,52 +6,52 @@ module.exports = {
   config: {
     // choose either `'stable'` for receiving highly polished,
     // or `'canary'` for less polished but more frequent updates
-    updateChannel: "stable",
+    updateChannel: 'stable',
     // default font size in pixels for all tabs
     fontSize: 14,
     // font family with optional fallbacks
     fontFamily:
       '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
     // default font weight: 'normal' or 'bold'
-    fontWeight: "normal",
+    fontWeight: 'normal',
     // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: "bold",
+    fontWeightBold: 'bold',
     // line height as a relative unit
     lineHeight: 1,
     // letter spacing as a relative unit
     letterSpacing: 0,
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: "rgba(229,233,240,1)",
+    cursorColor: 'rgba(229,233,240,1)',
     // terminal text color under BLOCK cursor
-    cursorAccentColor: "#000",
+    cursorAccentColor: '#000',
     // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-    cursorShape: "BLOCK",
+    cursorShape: 'BLOCK',
     // set to `true` (without backticks and without quotes) for blinking cursor
     cursorBlink: true,
     // color of the text
-    foregroundColor: "#fff",
+    foregroundColor: '#fff',
     // terminal background color
     // opacity is only supported on macOS
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     // terminal selection color
-    selectionColor: "rgba(229,233,240,0.3)",
+    selectionColor: 'rgba(229,233,240,0.3)',
     // border color (window, tabs)
-    borderColor: "#333",
+    borderColor: '#333',
     // custom CSS to embed in the main window
-    css: "",
+    css: '',
     // custom CSS to embed in the terminal window
-    termCSS: "",
+    termCSS: '',
     // set custom startup directory (must be an absolute path)
-    workingDirectory: "",
+    workingDirectory: '',
     // if you're using a Linux setup which show native menus, set to false
     // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: "",
+    showHamburgerMenu: '',
     // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: "",
+    showWindowControls: '',
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: "12px 14px",
+    padding: '12px 14px',
 
     // Hyper Window Size
     windowSize: [1000, 600],
@@ -60,35 +60,34 @@ module.exports = {
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: "#000000",
-      red: "#C51E14",
-      green: "#1DC121",
-      yellow: "#C7C329",
-      blue: "#0A2FC4",
-      magenta: "#C839C5",
-      cyan: "#20C5C6",
-      white: "#C7C7C7",
-      lightBlack: "#686868",
-      lightRed: "#FD6F6B",
-      lightGreen: "#67F86F",
-      lightYellow: "#FFFA72",
-      lightBlue: "#6A76FB",
-      lightMagenta: "#FD7CFC",
-      lightCyan: "#68FDFE",
-      lightWhite: "#FFFFFF",
-      limeGreen: "#32CD32",
-      lightCoral: "#F08080",
+      black: '#000000',
+      red: '#C51E14',
+      green: '#1DC121',
+      yellow: '#C7C329',
+      blue: '#0A2FC4',
+      magenta: '#C839C5',
+      cyan: '#20C5C6',
+      white: '#C7C7C7',
+      lightBlack: '#686868',
+      lightRed: '#FD6F6B',
+      lightGreen: '#67F86F',
+      lightYellow: '#FFFA72',
+      lightBlue: '#6A76FB',
+      lightMagenta: '#FD7CFC',
+      lightCyan: '#68FDFE',
+      lightWhite: '#FFFFFF',
+      limeGreen: '#32CD32',
+      lightCoral: '#F08080',
     },
 
     // Hyper Border
     hyperBorder: {
-      borderColors: ["rgba(229,233,240,1)"],
-      borderWidth: "4px",
+      borderColors: ['#B7C5D3'],
     },
 
     // hypercwd
     hypercwd: {
-      initialWorkingDirectory: "C:/dev",
+      initialWorkingDirectory: 'C:/dev',
     },
 
     // Hyper Window Opacity
@@ -112,10 +111,12 @@ module.exports = {
     //
     // Cygwin
     // - Example: `C:\\cygwin64\\bin\\bash.exe`
-    shell: "C:\\Program Files\\Git\\bin\\bash.exe",
+    shell: 'C:\\Program Files\\Git\\bin\\bash.exe',
+    // shell: 'C:\\Windows\\System32\\wsl.exe',
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
-    shellArgs: ["--login"],
+    shellArgs: ['--login'],
+    // shellArgs: ['-d', 'Ubuntu', '--cd', '/mnt/c/dev'],
     // for environment variables
     env: {},
     // Supported Options:
@@ -125,7 +126,7 @@ module.exports = {
     // An absolute file path to a sound file on the machine.
     // bellSoundURL: '/path/to/sound/file',
     // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
+    copyOnSelect: true,
     // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
     defaultSSHApp: true,
     // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
@@ -134,13 +135,13 @@ module.exports = {
     // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
     // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
     // (inside tmux or vim with mouse mode enabled for example).
-    macOptionSelectionMode: "vertical",
+    macOptionSelectionMode: 'vertical',
     // Whether to use the WebGL renderer. Set it to false to use canvas-based
     // rendering (slower, but supports transparent backgrounds)
     webGLRenderer: true,
     // keypress required for weblink activation: [ctrl|alt|meta|shift]
     // todo: does not pick up config changes automatically, need to restart terminal :/
-    webLinksActivationKey: "",
+    webLinksActivationKey: '',
     // if `false` (without backticks and without quotes), Hyper will use ligatures provided by some fonts
     disableLigatures: true,
     // set to true to disable auto updates
@@ -158,14 +159,15 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    "nord-hyper",
-    "hyper-opacity",
-    "hyper-tabs-enhanced",
-    "hyperborder",
-    "hyper-blink",
-    "hypercwd",
-    "hyper-statusline-extended",
-    "hypernpm",
+    'hyper-city-lights',
+    'hyper-opacity',
+    'hyper-tabs-enhanced',
+    'hyperborder',
+    'hyper-blink',
+    'hypercwd',
+    'hyper-statusline-extended',
+    'hypernpm',
+    'hyper-pane',
   ],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
